c0a42a84980d0b25a617dab275214ae163a51710
christian <christian.heuchert@airista.com>
2023-12-04 12:26:32 -0500

    next mqtt repo to try

--- README.md
+++ README.md
@@ -1,99 +1,91 @@
----
+<!--
 title: MQTT
 weight: 4705
----
-# tibco-mqtt
-This trigger provides your flogo application the ability to start a flow via MQTT
+-->
 
+# MQTT
+
+This trigger allows you to listen to messages on MQTT.
 
 ## Installation
 
+### Flogo CLI
+
 ```bash
-flogo install github.com/TIBCOSoftware/flogo-contrib/trigger/mqtt
+flogo install github.com/project-flogo/edge-contrib/trigger/mqtt
 ```
 
-## Schema
-Settings, Outputs and Endpoint:
+## Configuration
+
+### Settings:
+
+| Name          | Type   | Description                       |
+| :------------ | :----- | :-------------------------------- |
+| broker        | string | The broker URL - **_REQUIRED_**   |
+| id            | string | The id of client - **_REQUIRED_** |
+| username      | string | The user's name                   |
+| password      | string | The user's password               |
+| store         | string | The store for message persistence |
+| cleanSession  | bool   | Clean session flag                |
+| keepAlive     | int    | Keep Alive time in seconds        |
+| autoReconnect | bool   | Enable Auto-Reconnect             |
+| sslConfig     | object | SSL configuration                 |
+
+#### _sslConfig_ Object:
+
+| Property      | Type   | Description                                             |
+| :------------ | :----- | :------------------------------------------------------ |
+| skipVerify    | bool   | Skip SSL validation, defaults to true                   |
+| useSystemCert | bool   | Use the systems root certificate file, defaults to true |
+| caFile        | string | The path to PEM encoded root certificates file          |
+| certFile      | string | The path to PEM encoded client certificate              |
+| keyFile       | string | The path to PEM encoded client key                      |
+
+_Note: used if broker URI is ssl_
+
+### Handler Settings
+
+| Name       | Type   | Description                             |
+| :--------- | :----- | :-------------------------------------- |
+| topic      | string | The topic to listen on - **_REQUIRED_** |
+| replyTopic | string | The topic to reply on                   |
+| qos        | int    | The Quality of Service                  |
+| retain     | bool   | Retain Messages                         |
+
+#### Topics
+
+MQTT wildcard syntax is supported. For example if the topic is '/x/+/y/#' then the `topicParams` `output` will be populated with the wildcard values. The first wildcard will be in `topicParams` with key '0' and the second with key '1'. Topic wildcards can also be given a name: '/x/+param1/y/#param2'. Then the names 'param1' and 'param2' can be used to access the wildcards in the `topicParams` `output`.
+
+### Output:
+
+| Name        | Type   | Description          |
+| :---------- | :----- | :------------------- |
+| message     | string | The message received |
+| topic       | string | The MQTT topic       |
+| topicParams | params | The topic parameters |
+
+### Reply:
+
+| Name | Type   | Description       |
+| :--- | :----- | :---------------- |
+| data | object | The data recieved |
+
+## Example
 
 ```json
 {
-  "settings":[
-      {
-        "name": "broker",
-        "type": "string"
-      },
-      {
-        "name": "id",
-        "type": "string"
-      },
-      {
-        "name": "user",
-        "type": "string"
-      },
-      {
-        "name": "password",
-        "type": "string"
-      },
-      {
-        "name": "store",
-        "type": "string"
-      },
-      {
-        "name": "topic",
-        "type": "string"
-      },
-      {
-        "name": "qos",
-        "type": "number"
-      },
-      {
-        "name": "cleansess",
-        "type": "boolean"
-      }
-    ],
-    "endpoint": {
-      "settings": [
-        {
-          "name": "topic",
-          "type": "string"
-        }
-      ]
+  "id": "mqtt-trigger",
+  "name": "Mqtt Trigger",
+  "ref": "github.com/project-flogo/edge-contrib/trigger/mqtt",
+  "settings": {
+    "broker": "tcp://localhost:1883",
+    "id": "client-1"
+  },
+  "handlers": {
+    "settings": {
+      "topic": "flogo",
+      "qos": "0"
     }
-}
-```
-
-## Example Configurations
-
-Triggers are configured via the triggers.json of your application. The following are some example configuration of the MQTT Trigger.
-
-### Start a flow
-Configure the Trigger to start "myflow". "settings" "topic" is the topic it uses to listen for incoming messages. So in this case the "endpoints" "settings" "topic" is "test_start" will start "myflow" flow. The incoming message payload has to define "replyTo" which is the the topic used to reply on.
-
-```json
-{
-  "triggers": [
-    {
-      "name": "flogo-mqtt",
-      "settings": {
-        "topic": "flogo/#",
-        "broker": "tcp://192.168.1.12:1883",
-        "id": "flogo",
-        "user": "",
-        "password": "",
-        "store": "",
-        "qos": "0",
-        "cleansess": "false"
-      },
-      "endpoints": [
-        {
-          "actionType": "flow",
-          "actionURI": "embedded://myflow",
-          "settings": {
-            "topic": "test_start"
-          }
-        }
-      ]
-    }
-  ]
+  }
 }
 ```
--- trigger.go
+++ trigger.go
@@ -4,120 +4,283 @@ import (
 	"context"
 	"encoding/json"
 	"strconv"
+	"strings"
 	"time"
 
-	"github.com/TIBCOSoftware/flogo-lib/core/data"
-	"github.com/TIBCOSoftware/flogo-lib/core/trigger"
-	"github.com/TIBCOSoftware/flogo-lib/logger"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/project-flogo/core/data/metadata"
+	"github.com/project-flogo/core/support/log"
+	"github.com/project-flogo/core/support/ssl"
+	"github.com/project-flogo/core/trigger"
 )
 
-// log is the default package logger
-var log = logger.GetLogger("trigger-flogo-mqtt")
+var triggerMd = trigger.NewMetadata(&Settings{}, &HandlerSettings{}, &Output{})
 
-// MqttTrigger is simple MQTT trigger
-type MqttTrigger struct {
-	metadata       *trigger.Metadata
-	client         mqtt.Client
-	config         *trigger.Config
-	handlers       []*trigger.Handler
-	topicToHandler map[string]*trigger.Handler
+func init() {
+	_ = trigger.Register(&Trigger{}, &Factory{})
 }
 
-//NewFactory create a new Trigger factory
-func NewFactory(md *trigger.Metadata) trigger.Factory {
-	return &MQTTFactory{metadata: md}
+// TokenType is a type of token
+type TokenType int
+
+const (
+	// Literal is a literal token type
+	Literal TokenType = iota
+	// SingleLevel is a single level wildcard
+	SingleLevel
+	// MultiLevel is a multi level wildcard
+	MultiLevel
+)
+
+// Token is a MQTT topic token
+type Token struct {
+	TokenType TokenType
+	Token     string
 }
 
-// MQTTFactory MQTT Trigger factory
-type MQTTFactory struct {
-	metadata *trigger.Metadata
+// Topic is a parsed topic
+type Topic []Token
+
+// ParseTopic parses the topic
+func ParseTopic(topic string) Topic {
+	var parsed Topic
+	parts, index := strings.Split(topic, "/"), 0
+	for _, part := range parts {
+		if strings.HasPrefix(part, "+") {
+			token := strings.TrimPrefix(part, "+")
+			if token == "" {
+				token = strconv.Itoa(index)
+				index++
+			}
+			parsed = append(parsed, Token{
+				TokenType: SingleLevel,
+				Token:     token,
+			})
+		} else if strings.HasPrefix(part, "#") {
+			token := strings.TrimPrefix(part, "#")
+			if token == "" {
+				token = strconv.Itoa(index)
+				index++
+			}
+			parsed = append(parsed, Token{
+				TokenType: MultiLevel,
+				Token:     token,
+			})
+		} else {
+			parsed = append(parsed, Token{
+				TokenType: Literal,
+				Token:     part,
+			})
+		}
+	}
+	return parsed
 }
 
-//New Creates a new trigger instance for a given id
-func (t *MQTTFactory) New(config *trigger.Config) trigger.Trigger {
-	return &MqttTrigger{metadata: t.metadata, config: config}
+// Match matches the topic with an input topic
+func (t Topic) Match(input Topic) map[string]string {
+	output, i := make(map[string]string), 0
+MATCHER:
+	for _, token := range t {
+		switch token.TokenType {
+		case Literal:
+			if i > len(input) {
+				break MATCHER
+			}
+			if token.Token != input[i].Token {
+				break MATCHER
+			}
+		case SingleLevel:
+			if i >= len(input) {
+				output[token.Token] = ""
+				break MATCHER
+			}
+			output[token.Token] = input[i].Token
+		case MultiLevel:
+			if i >= len(input) {
+				output[token.Token] = ""
+				break MATCHER
+			}
+			levels, sep := "", ""
+			for _, level := range input[i:] {
+				levels += sep + level.Token
+				sep = "/"
+				i++
+			}
+			output[token.Token] = levels
+			break MATCHER
+		}
+		i++
+	}
+	return output
 }
 
-// Metadata implements trigger.Trigger.Metadata
-func (t *MqttTrigger) Metadata() *trigger.Metadata {
-	return t.metadata
+// String generates a string for the topic
+func (t Topic) String() string {
+	output := strings.Builder{}
+	for i, token := range t {
+		if i > 0 {
+			output.WriteString("/")
+		}
+		switch token.TokenType {
+		case Literal:
+			output.WriteString(token.Token)
+		case SingleLevel:
+			output.WriteString("+")
+		case MultiLevel:
+			output.WriteString("#")
+		}
+	}
+	return output.String()
+}
+
+// Trigger is simple MQTT trigger
+type Trigger struct {
+	handlers map[string]*clientHandler
+	settings *Settings
+	logger   log.Logger
+	options  *mqtt.ClientOptions
+	client   mqtt.Client
+}
+type clientHandler struct {
+	//client mqtt.Client
+	handler  trigger.Handler
+	settings *HandlerSettings
+}
+type Factory struct {
+}
+
+func (*Factory) Metadata() *trigger.Metadata {
+	return triggerMd
+}
+
+// New implements trigger.Factory.New
+func (*Factory) New(config *trigger.Config) (trigger.Trigger, error) {
+	s := &Settings{}
+
+	err := metadata.MapToStruct(config.Settings, s, true)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Trigger{settings: s}, nil
 }
 
 // Initialize implements trigger.Initializable.Initialize
-func (t *MqttTrigger) Initialize(ctx trigger.InitContext) error {
-	t.handlers = ctx.GetHandlers()
+func (t *Trigger) Initialize(ctx trigger.InitContext) error {
+	t.logger = ctx.Logger()
+
+	settings := t.settings
+	options := initClientOption(settings)
+	t.options = options
+
+	if strings.HasPrefix(settings.Broker, "ssl") {
+
+		cfg := &ssl.Config{}
+
+		if len(settings.SSLConfig) != 0 {
+			err := cfg.FromMap(settings.SSLConfig)
+			if err != nil {
+				return err
+			}
+
+			if _, set := settings.SSLConfig["skipVerify"]; !set {
+				cfg.SkipVerify = true
+			}
+			if _, set := settings.SSLConfig["useSystemCert"]; !set {
+				cfg.UseSystemCert = true
+			}
+		} else {
+			//using ssl but not configured, use defaults
+			cfg.SkipVerify = true
+			cfg.UseSystemCert = true
+		}
+
+		tlsConfig, err := ssl.NewClientTLSConfig(cfg)
+		if err != nil {
+			return err
+		}
+
+		options.SetTLSConfig(tlsConfig)
+	}
+
+	options.SetDefaultPublishHandler(func(client mqtt.Client, msg mqtt.Message) {
+		t.logger.Warnf("Recieved message on unhandled topic: %s", msg.Topic())
+	})
+
+	t.logger.Debugf("Client options: %v", options)
+
+	t.handlers = make(map[string]*clientHandler)
+
+	for _, handler := range ctx.GetHandlers() {
+
+		s := &HandlerSettings{}
+		err := metadata.MapToStruct(handler.Settings(), s, true)
+		if err != nil {
+			return err
+		}
+
+		t.handlers[s.Topic] = &clientHandler{handler: handler, settings: s}
+	}
+
 	return nil
 }
 
-// Start implements trigger.Trigger.Start
-func (t *MqttTrigger) Start() error {
+func initClientOption(settings *Settings) *mqtt.ClientOptions {
 
 	opts := mqtt.NewClientOptions()
-	opts.AddBroker(t.config.GetSetting("broker"))
-	opts.SetClientID(t.config.GetSetting("id"))
-	opts.SetUsername(t.config.GetSetting("user"))
-	opts.SetPassword(t.config.GetSetting("password"))
-	b, err := data.CoerceToBoolean(t.config.Settings["cleansess"])
-	if err != nil {
-		log.Error("Error converting \"cleansess\" to a boolean ", err.Error())
-		return err
-	}
-	opts.SetCleanSession(b)
-	if storeType := t.config.Settings["store"]; storeType != ":memory:" {
-		opts.SetStore(mqtt.NewFileStore(t.config.GetSetting("store")))
+	opts.AddBroker(settings.Broker)
+	opts.SetClientID(settings.Id)
+	opts.SetUsername(settings.Username)
+	opts.SetPassword(settings.Password)
+	opts.SetCleanSession(settings.CleanSession)
+	opts.SetAutoReconnect(settings.AutoReconnect)
+
+	if settings.Store != ":memory:" && settings.Store != "" {
+		opts.SetStore(mqtt.NewFileStore(settings.Store))
 	}
 
-	opts.SetDefaultPublishHandler(func(client mqtt.Client, msg mqtt.Message) {
-		topic := msg.Topic()
-		//TODO we should handle other types, since mqtt message format are data-agnostic
-		payload := string(msg.Payload())
-		log.Debug("Received msg:", payload)
-		handler, found := t.topicToHandler[topic]
-		if found {
-			t.RunHandler(handler, payload)
-		} else {
-			log.Errorf("handler for topic '%s' not found", topic)
-		}
-	})
+	if settings.KeepAlive != 0 {
+		opts.SetKeepAlive(time.Duration(settings.KeepAlive) * time.Second)
+	} else {
+		opts.SetKeepAlive(2 * time.Second)
+	}
 
-	client := mqtt.NewClient(opts)
-	t.client = client
+	return opts
+}
+
+// Start implements trigger.Trigger.Start
+func (t *Trigger) Start() error {
+
+	client := mqtt.NewClient(t.options)
+
 	if token := client.Connect(); token.Wait() && token.Error() != nil {
-		panic(token.Error())
+		return token.Error()
 	}
 
-	i, err := data.CoerceToDouble(t.config.Settings["qos"])
-	if err != nil {
-		log.Error("Error converting \"qos\" to an integer ", err.Error())
-		return err
-	}
-
-	t.topicToHandler = make(map[string]*trigger.Handler)
+	t.client = client
 
 	for _, handler := range t.handlers {
-
-		topic := handler.GetStringSetting("topic")
-
-		if token := t.client.Subscribe(topic, byte(i), nil); token.Wait() && token.Error() != nil {
-			log.Errorf("Error subscribing to topic %s: %s", topic, token.Error())
+		parsed := ParseTopic(handler.settings.Topic)
+		if token := client.Subscribe(parsed.String(), byte(handler.settings.Qos), t.getHanlder(handler, parsed)); token.Wait() && token.Error() != nil {
+			t.logger.Errorf("Error subscribing to topic %s: %s", handler.settings.Topic, token.Error())
 			return token.Error()
-		} else {
-			log.Debugf("Subscribed to topic: %s, will trigger handler: %s", topic, handler)
-			t.topicToHandler[topic] = handler
 		}
+
+		t.logger.Debugf("Subscribed to topic: %s", handler.settings.Topic)
 	}
 
 	return nil
 }
 
 // Stop implements ext.Trigger.Stop
-func (t *MqttTrigger) Stop() error {
-	//unsubscribe from topic
-	for _, handlerCfg := range t.config.Handlers {
-		log.Debug("Unsubscribing from topic: ", handlerCfg.GetSetting("topic"))
-		if token := t.client.Unsubscribe(handlerCfg.GetSetting("topic")); token.Wait() && token.Error() != nil {
-			log.Errorf("Error unsubscribing from topic %s: %s", handlerCfg.Settings["topic"], token.Error())
+func (t *Trigger) Stop() error {
+
+	//unsubscribe from topics
+	for _, handler := range t.handlers {
+		topic := ParseTopic(handler.settings.Topic).String()
+		t.logger.Debug("Unsubscribing from topic: ", topic)
+		if token := t.client.Unsubscribe(topic); token.Wait() && token.Error() != nil {
+			t.logger.Errorf("Error unsubscribing from topic %s: %s", topic, token.Error())
 		}
 	}
 
@@ -126,61 +289,57 @@ func (t *MqttTrigger) Stop() error {
 	return nil
 }
 
-// RunHandler runs the handler and associated action
-func (t *MqttTrigger) RunHandler(handler *trigger.Handler, payload string) {
+func (t *Trigger) getHanlder(handler *clientHandler, parsed Topic) func(mqtt.Client, mqtt.Message) {
+	return func(client mqtt.Client, msg mqtt.Message) {
+		topic := msg.Topic()
+		qos := msg.Qos()
+		payload := string(msg.Payload())
+		params := parsed.Match(ParseTopic(topic))
 
-	trgData := make(map[string]interface{})
-	trgData["message"] = payload
+		t.logger.Debugf("Topic[%s] - Payload Recieved: %s", topic, payload)
 
-	results, err := handler.Handle(context.Background(), trgData)
-
-	if err != nil {
-		log.Error("Error starting action: ", err.Error())
-	}
-
-	log.Debugf("Ran Handler: [%s]", handler)
-
-	var replyData interface{}
-
-	if len(results) != 0 {
-		dataAttr, ok := results["data"]
-		if ok {
-			replyData = dataAttr.Value()
-		}
-	}
-
-	if replyData != nil {
-		dataJson, err := json.Marshal(replyData)
+		result, err := runHandler(handler.handler, payload, topic, params)
 		if err != nil {
-			log.Error(err)
-		} else {
-			replyTo := handler.GetStringSetting("topic")
-			if replyTo != "" {
-				t.publishMessage(replyTo, string(dataJson))
+			t.logger.Error("Error handling message: %v", err)
+			return
+		}
+
+		if handler.settings.ReplyTopic != "" {
+			reply := &Reply{}
+			err = reply.FromMap(result)
+			if err != nil {
+				t.logger.Error("Error handling message: %v", err)
+				return
+			}
+
+			if reply.Data != nil {
+				dataJson, err := json.Marshal(reply.Data)
+				if err != nil {
+					return
+				}
+				token := client.Publish(handler.settings.ReplyTopic, qos, handler.settings.Retain, string(dataJson))
+				sent := token.WaitTimeout(5000 * time.Millisecond)
+				if !sent {
+					t.logger.Errorf("Timeout occurred while trying to publish reply to topic '%s'", handler.settings.ReplyTopic)
+					return
+				}
 			}
 		}
 	}
 }
 
-func (t *MqttTrigger) publishMessage(topic string, message string) {
+// RunHandler runs the handler and associated action
+func runHandler(handler trigger.Handler, payload, topic string, params map[string]string) (map[string]interface{}, error) {
+	out := &Output{
+		Message:     payload,
+		Topic:       topic,
+		TopicParams: params,
+	}
 
-	log.Debug("ReplyTo topic: ", topic)
-	log.Debug("Publishing message: ", message)
-
-	qos, err := strconv.Atoi(t.config.GetSetting("qos"))
+	results, err := handler.Handle(context.Background(), out)
 	if err != nil {
-		log.Error("Error converting \"qos\" to an integer ", err.Error())
-		return
+		return nil, err
 	}
-	if len(topic) == 0 {
-		log.Warn("Invalid empty topic to publish to")
-		return
-	}
-	token := t.client.Publish(topic, byte(qos), false, message)
-	sent := token.WaitTimeout(5000 * time.Millisecond)
-	if !sent {
-		// Timeout occurred
-		log.Errorf("Timeout occurred while trying to publish to topic '%s'", topic)
-		return
-	}
+
+	return results, nil
 }
