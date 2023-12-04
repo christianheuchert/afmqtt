5ca3760e06d18a65dbc00917468179fc67761cd2
christian <christian.heuchert@airista.com>
2023-12-04 11:43:17 -0500

    5

--- trigger.go
+++ trigger.go
@@ -1,11 +1,14 @@
 package mqtt
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"os"
+	"strconv"
 	"time"
 
+	"github.com/TIBCOSoftware/flogo-lib/core/data"
 	"github.com/TIBCOSoftware/flogo-lib/core/trigger"
 	"github.com/TIBCOSoftware/flogo-lib/logger"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
@@ -58,11 +61,39 @@ func (t *MqttTrigger) Start() error {
 	opts.SetClientID(config.BrokerId)
 	opts.SetUsername(config.BrokerUsername)
 	opts.SetPassword(config.BrokerPassword)
+	b, err := data.CoerceToBoolean(t.config.Settings["cleansess"])
+	if err != nil {
+		log.Error("Error converting \"cleansess\" to a boolean ", err.Error())
+		return err
+	}
+	opts.SetCleanSession(b)
+	if storeType := t.config.Settings["store"]; storeType != ":memory:" {
+		opts.SetStore(mqtt.NewFileStore(t.config.GetSetting("store")))
+	}
+
+	opts.SetDefaultPublishHandler(func(client mqtt.Client, msg mqtt.Message) {
+		topic := msg.Topic()
+		//TODO we should handle other types, since mqtt message format are data-agnostic
+		payload := string(msg.Payload())
+		log.Debug("Received msg:", payload)
+		handler, found := t.topicToHandler[topic]
+		if found {
+			t.RunHandler(handler, payload)
+		} else {
+			log.Errorf("handler for topic '%s' not found", topic)
+		}
+	})
 
 	client := mqtt.NewClient(opts)
 	t.client = client
 	if token := client.Connect(); token.Wait() && token.Error() != nil {
 		panic(token.Error())
+	}
+
+	i, err := data.CoerceToDouble(t.config.Settings["qos"])
+	if err != nil {
+		log.Error("Error converting \"qos\" to an integer ", err.Error())
+		return err
 	}
 
 	t.topicToHandler = make(map[string]*trigger.Handler)
@@ -71,7 +102,7 @@ func (t *MqttTrigger) Start() error {
 
 		topic := config.BrokerTopic
 
-		if token := t.client.Subscribe(topic, 0, nil); token.Wait() && token.Error() != nil {
+		if token := t.client.Subscribe(topic, byte(i), nil); token.Wait() && token.Error() != nil {
 			log.Errorf("Error subscribing to topic %s: %s", topic, token.Error())
 			return token.Error()
 		} else {
@@ -98,7 +129,66 @@ func (t *MqttTrigger) Stop() error {
 	return nil
 }
 
+// RunHandler runs the handler and associated action
+func (t *MqttTrigger) RunHandler(handler *trigger.Handler, payload string) {
 
+	trgData := make(map[string]interface{})
+	trgData["message"] = payload
+
+	results, err := handler.Handle(context.Background(), trgData)
+
+	if err != nil {
+		log.Error("Error starting action: ", err.Error())
+	}
+
+	log.Debugf("Ran Handler: [%s]", handler)
+
+	var replyData interface{}
+
+	if len(results) != 0 {
+		dataAttr, ok := results["data"]
+		if ok {
+			replyData = dataAttr.Value()
+		}
+	}
+
+	if replyData != nil {
+		dataJson, err := json.Marshal(replyData)
+		if err != nil {
+			log.Error(err)
+		} else {
+			replyTo := handler.GetStringSetting("topic")
+			if replyTo != "" {
+				t.publishMessage(replyTo, string(dataJson))
+			}
+		}
+	}
+}
+
+func (t *MqttTrigger) publishMessage(topic string, message string) {
+
+	log.Debug("ReplyTo topic: ", topic)
+	log.Debug("Publishing message: ", message)
+
+	qos, err := strconv.Atoi(t.config.GetSetting("qos"))
+	if err != nil {
+		log.Error("Error converting \"qos\" to an integer ", err.Error())
+		return
+	}
+	if len(topic) == 0 {
+		log.Warn("Invalid empty topic to publish to")
+		return
+	}
+	token := t.client.Publish(topic, byte(qos), false, message)
+	sent := token.WaitTimeout(5000 * time.Millisecond)
+	if !sent {
+		// Timeout occurred
+		log.Errorf("Timeout occurred while trying to publish to topic '%s'", topic)
+		return
+	}
+}
+
+// 	config := readConfig("mqtt.json") // config data
 func readConfig(configPath string) Config {
 	var config Config
 
